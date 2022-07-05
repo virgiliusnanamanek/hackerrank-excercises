eb63d59ccaee554375efadff3789536db1d75030
virgiliusnanamanek <vickynana034@gmail.com>
2022-07-05 22:10:05 +0700

    Create counting-sort1.js

---
+++ day-two/counting-sort1.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'countingSort' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function countingSort(arr) {
+    // Write your code here
+    const count = [];
+    for(let i = 0; i < 100; i++){
+        count[0] = 0;
+    }
+    for(let i = 0; i < arr.length; i++){
+        count[arr[i]]++;
+    }
+
+    const processData = input =>{
+        const line = input.split('\n');
+        const arr = line.split(' ').map(arrTemp => parseInt(arrTemp));
+        countingSort(arr);
+    }
+
+    return count[i];
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = countingSort(arr);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
