15cedc38e7382ac8011057f0b34497a7c89697da
virgiliusnanamanek <vickynana034@gmail.com>
2022-07-09 00:39:25 +0700

    Update flipping-matrix.js

--- day-two/flipping-matrix.js
+++ day-two/flipping-matrix.js
@@ -10,18 +10,18 @@
 
 function flippingMatrix(matrix) {
     // your code here
-    let n = matrix.length/2;
-    let max = 0;
+    let n = matrix.length/2; // n is the length of the matrix
+    let max = 0; 
     let sum = 0;
     
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            max = Number.MIN_VALUE;
-            max = Math.max(max, matrix[i][j]);
-            max = Math.max(max, matrix[i][2*n-j-1]);
-            max = Math.max(max, matrix[2*n-i-1][j]);
-            max = Math.max(max, matrix[2*n-i-1][2*n-j-1]);
-            sum += max;
+    for (let i = 0; i < n; i++) { // loop through the matrix
+        for (let j = 0; j < n; j++) { // loop through the matrix
+            max = Number.MIN_VALUE; // reset max
+            max = Math.max(max, matrix[i][j]); // get the max of the current row
+            max = Math.max(max, matrix[i][2*n-j-1]); // get the max of the current column
+            max = Math.max(max, matrix[2*n-i-1][j]); // get the max of the current diagonal
+            max = Math.max(max, matrix[2*n-i-1][2*n-j-1]); // get the max of the current
+            sum += max; // add the max to the sum
         }
     }
     return sum;
