23f50591fad249cd326eeb66bd7bb0e191fbd22b
virgiliusnanamanek <vickynana034@gmail.com>
2022-07-09 15:16:51 +0700

    Create sumof-array.js

---
+++ problem-sloving/sumof-array.js
@@ -0,0 +1,11 @@
+// function to sum array
+
+function sumOfArray(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+console.log(sumOfArray([1, 2, 3, 4, 5]));
