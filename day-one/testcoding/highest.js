bf11affc8faeb9a481b88af77e853a4930d352a8
virgiliusnanamanek <vickynana034@gmail.com>
2022-07-11 11:22:33 +0700

    Create highest.js

---
+++ day-one/testcoding/highest.js
@@ -0,0 +1,13 @@
+// count how many numbers are highest in the array
+
+function highest(arr) {
+    let highest = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > highest) {
+            highest = arr[i];
+        }
+    }
+    return highest;
+}
+
+console.log(highest([1,2,3,4,5]));
