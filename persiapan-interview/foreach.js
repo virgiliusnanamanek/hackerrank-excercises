c89f01c3c8f657d6f6cf3dc231f47a3d26ead140
virgiliusnanamanek <vickynana034@gmail.com>
2022-07-17 22:37:44 +0700

    Create foreach.js

---
+++ persiapan-interview/foreach.js
@@ -0,0 +1,11 @@
+const a = [1, 2, 3, 4]
+const double = a.forEach(function (item) {
+  return item * 2
+})
+
+console.log(double)
+
+const doub = a.map(function (item) {
+  return item * 2
+});
+console.log(doub)
