115604af70ca494b20b12deeae1724dab8357891
virgiliusnanamanek <vickynana034@gmail.com>
2022-07-20 22:58:21 +0700

    Update higher-order-function.js

--- interview-softwareseni/higher-order-function.js
+++ interview-softwareseni/higher-order-function.js
@@ -1,5 +1,9 @@
 // create higher order function
 
+/**
+ * Higher-order function atau fungsi orde tinggi adalah fungsi yang 1 menerima fungsi lain sebagai parameternya, dan/atau 2) mengembalikan fungsi lain sebagai keluarannya.
+ */
+
 function filter(arr, callback) {
   let newArr = [];
   for (let i = 0; i < arr.length; i++) {
