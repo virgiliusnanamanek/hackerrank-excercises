de8876ae669b7f47ec3a9ad34792bbac7ca59203
virgiliusnanamanek <vickynana034@gmail.com>
2022-07-22 13:03:20 +0700

    Update sort.js

--- interview-softwareseni/sort.js
+++ interview-softwareseni/sort.js
@@ -1,17 +1,18 @@
 // sorting array without sort() method
 //console.log(sorting([2,1,4,3,1])); must return [1,1,2,3,4]
 
-    function sorting(arr) {
-        let newArr = [];
-        for (let i = 0; i < arr.length; i++) {
-            for (let j = 0; j < arr.length; j++) {
-                if (arr[i] < arr[j]) {
-                    newArr.push(arr[i]);
-                    newArr.push(arr[j]);
-                }
+function sorting(arr){
+    var temp;
+    for(var i=0; i<arr.length; i++){
+        for(var j=i+1; j<arr.length; j++){
+            if(arr[i]>arr[j]){
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
             }
         }
-        return newArr;
     }
+    return arr;
+}
 
-    console.log(sorting([2,1,4,3,1]));
+console.log(sorting([2,1,4,3,1]));
